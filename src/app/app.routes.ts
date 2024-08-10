0a46204bafcd5c8222cb972584841b49f8e09e20
Amankhare01 <amankhare.aa@gmail.com>
2024-08-10 16:23:21 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { RegisterComponent } from './component/register/register.component';
+
+export const routes: Routes = [
+  {
+    path:'register',
+    component: RegisterComponent
+  }
+];
